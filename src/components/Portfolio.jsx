c5dd1865e283cb9236c47a6f8fb9aac4754de7f1
fatimamahlaba <fatimamahlaba@gmail.com>
2022-11-17 14:23:59 +0200

    second commit

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -4,34 +4,127 @@ import "swiper/css";
 import "swiper/css/pagination";
 import { Pagination, Autoplay } from "swiper";
 
-function Portfolio() {
+const Portfolio = () => {
+
+  const projects = [
+    {
+      img: "https://cdn.vox-cdn.com/thumbor/w-IFN0FWpN4BGfhZaV9EYqs4nLo=/51x0:977x617/1200x800/filters:focal(51x0:977x617)/cdn.vox-cdn.com/uploads/chorus_image/image/50017015/Screen_Shot_2016-07-04_at_12.37.15_PM.0.0.png",
+      name: "React",
+      github_link: "https://github.com/Sridhar-C-25",
+      live_link: "https://myreactflix.netlify.app",
+    },
+    {
+      img: "https://cdn.vox-cdn.com/thumbor/w-IFN0FWpN4BGfhZaV9EYqs4nLo=/51x0:977x617/1200x800/filters:focal(51x0:977x617)/cdn.vox-cdn.com/uploads/chorus_image/image/50017015/Screen_Shot_2016-07-04_at_12.37.15_PM.0.0.png",
+      name: "Job",
+      github_link: "https://github.com/Sridhar-C-25/jobsearchapp",
+      live_link: "https://myjobsearch.netlify.app",
+    },
+    {
+      img: "https://cdn.vox-cdn.com/thumbor/w-IFN0FWpN4BGfhZaV9EYqs4nLo=/51x0:977x617/1200x800/filters:focal(51x0:977x617)/cdn.vox-cdn.com/uploads/chorus_image/image/50017015/Screen_Shot_2016-07-04_at_12.37.15_PM.0.0.png",
+      name: "Highking",
+      github_link: "https://github.com/Sridhar-C-25/highking",
+      live_link: "https://highking01.netlify.app",
+    },
+    {
+      img: "https://cdn.vox-cdn.com/thumbor/w-IFN0FWpN4BGfhZaV9EYqs4nLo=/51x0:977x617/1200x800/filters:focal(51x0:977x617)/cdn.vox-cdn.com/uploads/chorus_image/image/50017015/Screen_Shot_2016-07-04_at_12.37.15_PM.0.0.png",
+      name: "Nav",
+      github_link:
+        "https://github.com/Sridhar-C-25/reacttailwindnavbar-with-dropdown",
+      live_link: "https://reacttailwindnavbar.netlify.app",
+    },
+    {
+      img: "https://cdn.vox-cdn.com/thumbor/w-IFN0FWpN4BGfhZaV9EYqs4nLo=/51x0:977x617/1200x800/filters:focal(51x0:977x617)/cdn.vox-cdn.com/uploads/chorus_image/image/50017015/Screen_Shot_2016-07-04_at_12.37.15_PM.0.0.png",
+      name: "Vue",
+      github_link: "https://github.com/Sridhar-C-25",
+      live_link: "https://vuecountry05.netlify.app",
+    },
+  ];
   return(
-    <section
-    id="Intro"
-    className="flex items-center justify-center flex-col pt-20 pb-6"
-    >
-  <div class="project-container">
-  <div class="project">
-    <div class="project-content">
-      
-      <h4 class="project-title">A Clash of Kings</h4>
-      <div class="project-details">
-        <p>Lorem ipsum dolor amet you probably haven't heard of them bitters selvage listicle heirloom. Locavore kombucha street art ennui 90's, organic food truck hell of seitan skateboard literally hexagon fixie next level. Lomo salvia yuccie hella roof party echo park vegan four dollar toast cred.</p>
-        <ul>
-          <li>Shadow</li>
-          <li>Demon</li>
-          <li>Baby</li>
-        </ul>
+    <section id="portfolio" className="py-10 text-white">
+      <div className="text-center">
+        <h3 className="text-4xl font-semibold">
+          My <span className="text-cyan-600">Projects</span>
+        </h3>
+        <p className="text-gray-400 mt-3 text-lg">My awesome works</p>
       </div>
-    </div>
+      <br />
+      
+        <div className="">
+          <Swiper
+            slidesPreview={1.2}
+            spaceBetween={20}
+            breakpoints={{
+              768: {
+                slidesPerView: 2,
+              },
+            }}
+            loop={true}
+            autoplay={{
+              delay: 3000,
+            }}
+            pagination={{
+              clickable: true,
+            }}
+            modules={[Pagination, Autoplay]}
+          >
+            {projects.map((project_info, i) => (
+              <SwiperSlide key={i}>
+
+<div className="project-container">
+  <div className="project">
+     <div className="project-content">
+      
+       <h4 className="project-title">{project_info.name}</h4>
+       <div className="project-details">
+         <p>Lorem ipsum dolor amet you probably haven't heard of them bitters selvage listicle heirloom. Locavore kombucha street art ennui 90's, organic food truck hell of seitan skateboard literally hexagon fixie next level. Lomo salvia yuccie hella roof party echo park vegan four dollar toast cred.</p>
+         <ul>
+           <li href={project_info.github_link}
+                      target="_blank">Shadow</li>
+           <li>Demon</li>
+           <li>Baby</li>
+         </ul>
+       </div>
+     </div>
     
-    <div class="project-img">
-      <img src="https://cdn.vox-cdn.com/thumbor/w-IFN0FWpN4BGfhZaV9EYqs4nLo=/51x0:977x617/1200x800/filters:focal(51x0:977x617)/cdn.vox-cdn.com/uploads/chorus_image/image/50017015/Screen_Shot_2016-07-04_at_12.37.15_PM.0.0.png" alt="" />
-    </div>
+     <div class="project-img">
+       <img src= {project_info.img} alt="" />
+     </div>
     
-  </div>
-</div>
+   </div>
+ </div><br />
+     </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+
+
     </section>
+//     <section
+//     id="Intro"
+//     className="flex items-center justify-center flex-col pt-20 pb-6"
+//     >
+//   <div class="project-container">
+//   <div class="project">
+//     <div class="project-content">
+      
+//       <h4 class="project-title">A Clash of Kings</h4>
+//       <div class="project-details">
+//         <p>Lorem ipsum dolor amet you probably haven't heard of them bitters selvage listicle heirloom. Locavore kombucha street art ennui 90's, organic food truck hell of seitan skateboard literally hexagon fixie next level. Lomo salvia yuccie hella roof party echo park vegan four dollar toast cred.</p>
+//         <ul>
+//           <li>Shadow</li>
+//           <li>Demon</li>
+//           <li>Baby</li>
+//         </ul>
+//       </div>
+//     </div>
+    
+//     <div class="project-img">
+//       <img src="https://cdn.vox-cdn.com/thumbor/w-IFN0FWpN4BGfhZaV9EYqs4nLo=/51x0:977x617/1200x800/filters:focal(51x0:977x617)/cdn.vox-cdn.com/uploads/chorus_image/image/50017015/Screen_Shot_2016-07-04_at_12.37.15_PM.0.0.png" alt="" />
+//     </div>
+    
+//   </div>
+// </div>
+//     </section>
 )
   }
 
