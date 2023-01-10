922307fe5fbb8ef20f0e89525424d95f67fb7efc
fatimamahlaba <fatimamahlaba@gmail.com>
2023-01-10 10:56:07 +0200

    2023 changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
   <p className="">I look forward to hearing from you.Although I’m not currently looking for any new opportunities, my inbox is always open.</p>
     <p className="">Whether you have a question or just want to say hi, I’ll try my best to get back to you!</p>
     
-<a href="mailto:fatimamahlaba@gmail.com" className="cursor-pointer hover:bg-cyan-500 w-40 p-3 m-20 text-center border-2 border-cyan-600 active:text-white">
+<a href="mailto:fatimamahlaba@gmail.com" className="cursor-pointer rounded hover:bg-cyan-500 w-40 p-3 m-20 text-center border-2 border-cyan-600 active:text-white">
   Say Hello
 </a>
 </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="min-w-screen bg-gray-800 text-sm p-1 text-center text-white">
-      Copyright © 2022 Fatima Mahlaba Rights reserved.
+      Copyright © 2023 Fatima Mahlaba Rights reserved.
     </div>
   );
 };
